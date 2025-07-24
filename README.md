e66ae8a774a30fb15b2af8d0c214469f497092e0
Niket Shukla <niketshukla@gmail.com>
2025-07-24 12:59:21 -0700

    Basic React

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algo
+- Caching - Faster Builds
+- Image optimisation
+- Minification of Files
+- Bundling of Files
+- Compressing of Files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - To support older browsers diff bundle to run diff browsers
+- Good Error Handling
+- Service to run App on HTTPs
+- Tree Shaking Algo - Will remove unused code
+- Different Dev and Prod builds
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Hello from React"),
-    React.createElement("h2", { id: "heading" }, "Hello from React"),
-  ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element
+const parent = <h1 className="heading">This is React element using JSX</h1>;
+
+// React Component
+// Class Component - Old ways of creating components
+// Functional Component - New ways of creating components
+const TitleComponent = () => <h1 className="heading">Namaste React using JSX 🚀</h1>;
+
+const ParentComponent = () => {
+  return (
+    <div id="container">
+      <TitleComponent />
+      {/* Can be called like a function */}
+      {TitleComponent()}
+      {/* Calling a react element inside a component */}
+      {parent}
+      <h2 className="subHeading">This is a Functional Component</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(parent);
+root.render(<ParentComponent />);
